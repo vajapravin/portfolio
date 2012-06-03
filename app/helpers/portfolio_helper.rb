a09682dc0ca2783027fd6545a49919e0dfa09caa
jackknife <jackknife@jackknife-Lenovo-S10-3c.(none)>
2012-06-03 13:40:53 +0530

    portfolio is complete

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,2 +1,7 @@
 module PortfolioHelper
+  def gravatar_for
+    gravatar_id = Digest::MD5::hexdigest("vajapravin23@gmail.com".downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
+    image_tag(gravatar_url, alt: 'vajapravin', class: "photo")
+  end
 end
